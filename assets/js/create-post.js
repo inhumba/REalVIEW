761ffa6f3a770a99c444d484b48c5ee6a234c5ff
Pathompong Sangkajit <p.inhumba@gmail.com>
2022-12-09 04:35:38 +0900

    Feature: Add back button on create post review

--- assets/js/create-post.js
+++ assets/js/create-post.js
@@ -10,20 +10,61 @@ $(document).ready((function () {
         return $(".review--1").hide(), $(".review--2").show(), !1
     })), $(".next--to").click((function () {
         return $(".review--2").is(":visible") ? ($(".review--1, .review--2").hide(), $(".review--3").show(), $(this).html("โพสต์")) : $(".review--1, .review--2").show(), !1
-    })), $(document).on("click", ".cancel", (function () {
+    })), $(document).on("click blur", ".cancel", (function () {
         $(".box--member").toggleClass("active")
     }))
+
+    $(".cancel-btn").click(function () {
+        $(".box--member").removeClass("active")
+    })
 
     var reviewIndex = 1
     $(".next--review").click(function () {
         $(".review--"+reviewIndex).hide()
         $(".review--"+(reviewIndex+1)).show()
         reviewIndex++
+        if($(".review--1").is(":visible")) {
+            $(this).html("ต่อไป")
+            $("body").removeClass("bg-bg")
+            $(".topic").html("สร้างรีวิว")
+            $(".back-btn").hide()
+            $(".cancel-btn").show()
+        }
+        if($(".review--2").is(":visible")) {
+            $(this).html("ต่อไป")
+            $("body").removeClass("bg-bg")
+            $(".topic").html("สร้างรีวิว")
+            $(".back-btn").show()
+            $(".cancel-btn").hide()
+        }
         if($(".review--3").is(":visible")) {
             $(this).html("โพสต์ !")
             $("body").addClass("bg-bg")
             $(".topic").html("พรีวิวก่อนโพสต์")
-            reviewIndex = 4
+            $(".back-btn").show()
+            $(".cancel-btn").hide()
+        }
+        if(reviewIndex === 4) {
+            window.location.replace('home.html')
+        }
+    })
+    $(".back-btn").click(function () {
+        $(".review--"+reviewIndex).hide()
+        $(".review--"+(reviewIndex-1)).show()
+        reviewIndex = reviewIndex - 1
+        if($(".review--1").is(":visible")) {
+            $(".next--review").html("ต่อไป")
+            $("body").removeClass("bg-bg")
+            $(".topic").html("สร้างรีวิว")
+            $(".back-btn").hide()
+            $(".cancel-btn").show()
+        }
+        if($(".review--2").is(":visible")) {
+            $(".next--review").html("ต่อไป")
+            $("body").removeClass("bg-bg")
+            $(".topic").html("สร้างรีวิว")
+            $(".back-btn").show()
+            $(".cancel-btn").hide()
         }
     })
 }));
